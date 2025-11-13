d31d1f43083846a2c31316e320c19f8799c9cc08
pauline <178813716+paulinebat@users.noreply.github.com>
2025-11-13 09:22:28 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost'; // ou l'IP si ta base est distante
-$dbname = 'jeuxvideo'; // le nom de ta base dans DBeaver
-$username = 'root'; // ou ton identifiant
-$password = 'root';     // ton mot de passe MySQL
+$host = 'localhost'; // l'IP 
+$dbname = 'jeuxvideo'; // le nom de la base dans DBeaver
+$username = 'root'; // identifiant
+$password = 'root';     // mot de passe MySQL
 
 try {
   $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);

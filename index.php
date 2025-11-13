b2ffc2e640a7fd7bcbe3d133ba4d62686050db87
pauline <178813716+paulinebat@users.noreply.github.com>
2025-11-13 15:09:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <h1>Ajouter un utilisateur</h1>
+  <h1>Ajouter des jeux</h1>
 
   <form action="create.php" method="POST">
     <input type="text" name="nom" placeholder="Nom" required>
@@ -15,15 +15,23 @@
     <button type="submit">Ajouter</button>
   </form>
 
-  <h2>Liste des utilisateurs</h2>
+  <h2>Liste des jeux</h2>
   <?php
-    require 'db.php';
-    $users = $pdo->query("SELECT * FROM utilisateurs")->fetchAll();
+require 'db.php';
+$jeux = $pdo->query("
+  SELECT jeux.Jeux_Titre AS titre, genre.Genre_Id AS genre
+  FROM jeux
+  JOIN genre ON jeux.Genre_Id = genre.Genre_Id
+")->fetchAll();
 
-    foreach ($users as $user) {
-      echo "<p><strong>" . htmlspecialchars($user['nom']) . "</strong> – " . htmlspecialchars($user['email']) . "</p>";
-    }
-  ?>
+
+foreach ($jeux as $jeu) {
+  $Jeux_Titre = $jeu['titre'] ?? 'Nom inconnu';
+  $Genre_Id = $jeu['genre'] ?? 'Genre inconnu';
+
+  echo "<p><strong>" . htmlspecialchars($Jeux_Titre) . "</strong> – " . htmlspecialchars($Genre_Id) . "</p>";
+}
+?>
 </body>
 </html>
 
